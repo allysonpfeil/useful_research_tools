9fa2b00585dd2a002f257d5fa1f8e63d190980f1
Ally N Pfeil <114772538+allysonpfeil@users.noreply.github.com>
2024-06-13 10:23:14 -0500

    Update data_extractor.py

--- data_extractor/data_extractor.py
+++ data_extractor/data_extractor.py
@@ -2,7 +2,6 @@ import os
 from docx import Document
 import re
 
-# extract text around the keyword match
 def extract_text_around_keyword(text, keyword, window=50): # change 50 to another number to signify how many characters to return
     match = re.search(rf'({keyword}|{keyword.lower()}|{keyword.upper()})\W*(\d+(\.\d+)?\s*\w*)?', text)
     if match:
@@ -10,8 +9,6 @@ def extract_text_around_keyword(text, keyword, window=50): # change 50 to anothe
         end = min(len(text), match.end() + window)
         return text[start:end]
     return None
-
-# search for keywords in a Word document
 def search_word_documents_for_keyword(folder_path, keyword):
     for filename in os.listdir(folder_path):
         if filename.endswith('.docx'):
@@ -23,8 +20,6 @@ def search_word_documents_for_keyword(folder_path, keyword):
                 print(f"File: {filename}")
                 print(extracted_text)
                 print("----")
-
 folder_path = "your/path/here"
 keyword = 'your keyword here'
-
 search_word_documents_for_keyword(folder_path, keyword)
